e560a529262d473735daa18d73ca57050ba0399d
Fuad Hasan <labib.knc@gmail.com>
2025-09-21 13:13:57 +0600

    Javascript datatypes

---
+++ JavaScript/dataTypes.js
@@ -0,0 +1,13 @@
+const a  = 'Hello';
+const b = 5;
+const c = true;
+const d = {firstName:"John", lastName:"Doe"};
+let e;
+const f = null;
+
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
+console.log(typeof(f));
