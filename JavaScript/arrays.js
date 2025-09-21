b467585bea446ea37faab13608376cd08896ba2e
Fuad Hasan <labib.knc@gmail.com>
2025-09-22 01:24:24 +0600

    Array Find

--- JavaScript/arrays.js
+++ JavaScript/arrays.js
@@ -67,3 +67,44 @@ console.log(cars);
 //Unshift
 cars.unshift("Saab");
 console.log(cars);
+//Concat
+const moreCars = ["Mercedes", "Toyota"];
+const allCars = cars.concat(moreCars);
+console.log(allCars);
+//CopyWithin
+const copyArray = [1, 2, 3, 4, 5];
+console.log(copyArray.copyWithin(0, 3)); // [4, 5, 3, 4, 5]
+//Flat
+const nestedArray = [1, 2, [3, 4], [5, [6, 7]]];
+console.log(nestedArray.flat()); // [1, 2, 3, 4, 5, [6, 7]]
+console.log(nestedArray.flat(2)); // [1, 2, 3, 4, 5, 6, 7]
+
+//FlatMap
+const words = ["Hello World", "JavaScript is fun"];
+const flattened = words.flatMap(word => word.split(" "));
+console.log(flattened); // ["Hello", "World", "JavaScript", "is", "fun"]
+//Splice
+const spliceArray = ["Jan", "March", "April", "June"];
+spliceArray.splice(1, 0, "Feb"); // Inserts "Feb" at index 1
+console.log(spliceArray); // ["Jan", "Feb", "March", "April", "June"]
+spliceArray.splice(4, 1, "May");
+
+//Slice
+const sliceArray = ["Jan", "Feb", "March", "April", "May"];
+const newArray = sliceArray.slice(1, 4); // ["Feb", "March", "April"]
+console.log(newArray);
+//Tostring
+console.log(sliceArray.toString()); // "Jan,Feb,March,April,May"
+
+//Array Search Methods
+console.log(sliceArray.indexOf("March")); // 2
+console.log(sliceArray.lastIndexOf("April")); // 3
+console.log(sliceArray.includes("adfasf")); // false
+
+//All about array find
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// Find the first even number
+const firstEven = numbers.find((num, index, numbers) => num === 2);
+console.log(firstEven); // Outputs: 2
+
