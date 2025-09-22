d2ea0a706c8b613bac31884f4adf0c86e66020f7
Fuad Hasan <labib.knc@gmail.com>
2025-09-22 19:52:22 +0600

    Different types of parameter in function

--- TypeScript/functions.ts
+++ TypeScript/functions.ts
@@ -19,3 +19,28 @@ console.log(greet("Fuad"));
 const greet2 = (name: string): string => {
   return "Hello " + name;
 };
+
+// Optional Parameters
+// Not all parameters are required. Add ?:
+
+function demo(name: string, title?: string): string {
+  return title ? `${title} ${name}` : `Hello ${name}`;
+}
+
+console.log(demo("Rahim")); // Hello Rahim
+console.log(demo("Rahim", "Mr.")); // Mr. Rahim
+
+//Default Parameters
+function demo2(name: string, title: string = "Mr."): string {
+  return `${title} ${name}`;
+}
+
+console.log(demo2("Rahim")); // Mr. Rahim
+console.log(demo2("Rahim", "Khan")); // Mr. Rahim
+
+//Rest Parameters
+function sum(...nums: number[]): number {
+  return nums.reduce((a, b) => a + b, 0);
+}
+
+console.log(sum(1, 2, 3, 4)); // 10
