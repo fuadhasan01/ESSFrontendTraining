78321a01b8c116828826087eb6a9486d47d8829f
Fuad Hasan <labib.knc@gmail.com>
2025-09-22 19:46:48 +0600

    Different types of function

--- TypeScript/functions.ts
+++ TypeScript/functions.ts
@@ -7,3 +7,15 @@
 function add(a: number, b: number): number {
   return a + b;
 }
+
+//assign a function to a variable:
+const greet = function (name: string): string {
+  return "Hello " + name;
+};
+
+console.log(greet("Fuad"));
+
+//Arrow Functions
+const greet2 = (name: string): string => {
+  return "Hello " + name;
+};
