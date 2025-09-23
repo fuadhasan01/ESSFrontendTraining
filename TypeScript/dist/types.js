5723b25ba59a5dcb21da09d9a7b711e3ddbbb5c7
Fuad Hasan <labib.knc@gmail.com>
2025-09-23 15:54:26 +0600

    Practiced all Types in TypeScript

---
+++ TypeScript/dist/types.js
@@ -0,0 +1,47 @@
+"use strict";
+//Basic Types
+Object.defineProperty(exports, "__esModule", { value: true });
+//Number
+let age = 21;
+let price = 99.5;
+//String
+let name = "Fuad";
+let country = "Bangladesh";
+//Boolean
+let isPassed = true;
+let isLoggedIn = false;
+//Any
+let data = 10;
+data = "Hello";
+data = true;
+//Array Types
+let numbers = [1, 2, 3, 4];
+let names = ["Ali", "Sami", "Rafi"];
+//Object Types
+let student = {
+    name: "Rafi",
+    age: 20,
+    passed: true,
+};
+//Union Types
+//A variable can have more than one possible type.
+let id;
+id = 101; // number ok
+id = "101A"; // string ok
+let user1 = 123;
+let user2 = "abc123";
+//Function Types
+// Functions also need types (input and output).
+function add(a, b) {
+    return a + b;
+}
+function greet(name) {
+    return "Hello, " + name;
+}
+//Special Types
+let example;
+example = null;
+function sayHello() {
+    console.log("Hello");
+}
+//# sourceMappingURL=types.js.map
---
+++ TypeScript/src/types.ts
@@ -0,0 +1,60 @@
+//Basic Types
+
+//Number
+let age: number = 21;
+let price: number = 99.5;
+
+//String
+let name: string = "Fuad";
+let country: string = "Bangladesh";
+
+//Boolean
+let isPassed: boolean = true;
+let isLoggedIn: boolean = false;
+
+//Any
+let data: any = 10;
+data = "Hello";
+data = true;
+
+//Array Types
+let numbers: number[] = [1, 2, 3, 4];
+let names: string[] = ["Ali", "Sami", "Rafi"];
+
+//Object Types
+let student: { name: string; age: number; passed: boolean } = {
+  name: "Rafi",
+  age: 20,
+  passed: true,
+};
+
+//Union Types
+//A variable can have more than one possible type.
+let id: string | number;
+
+id = 101; // number ok
+id = "101A"; // string ok
+
+//Type Aliases
+// You can create your own custom type.
+type UserID = string | number;
+
+let user1: UserID = 123;
+let user2: UserID = "abc123";
+
+//Function Types
+// Functions also need types (input and output).
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+function greet(name: string): string {
+  return "Hello, " + name;
+}
+
+//Special Types
+let example: string | null;
+example = null;
+function sayHello(): void {
+  console.log("Hello");
+}
