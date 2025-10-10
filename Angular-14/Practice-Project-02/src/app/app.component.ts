51c1a828b5062eca8245861448130d8aebf890a4
Fuad Hasan <labib.knc@gmail.com>
2025-10-10 20:03:51 +0600

    Parent to child binding

--- Angular-14/Practice-Project-02/src/app/app.component.ts
+++ Angular-14/Practice-Project-02/src/app/app.component.ts
@@ -10,7 +10,22 @@ interface ServerType {
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  serverElements: ServerType[] = [
-    { type: 'server', name: 'Test Server', content: 'Just a test!' },
-  ];
+  serverElements: ServerType[] = [];
+  onServerAdded(serverData: { serverName: string; serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent,
+    });
+  }
+  onBlueprintAdded(blueprintData: {
+    serverName: string;
+    serverContent: string;
+  }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent,
+    });
+  }
 }
