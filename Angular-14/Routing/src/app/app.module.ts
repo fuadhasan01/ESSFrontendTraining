5d9516228816f5e1d19ba2bebde5e76f19d746a8
Fuad Hasan <labib.knc@gmail.com>
2025-10-16 19:54:49 +0600

    Last commit 16/10/2025

--- Angular-14/Routing/src/app/app.module.ts
+++ Angular-14/Routing/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
 
 // import { AppRoutingModule } from './app-routing.module';
 
@@ -13,14 +12,10 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-
-const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'users', component: UsersComponent },
-  { path: 'users/:id/:name', component: UserComponent },
-  { path: 'servers', component: ServersComponent },
-  { path: 'servers/:id/edit', component: EditServerComponent },
-];
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './users/auth-guard.service';
 
 @NgModule({
   declarations: [
@@ -31,9 +26,10 @@ const appRoutes: Routes = [
     UserComponent,
     EditServerComponent,
     ServerComponent,
+    PageNotFoundComponent,
   ],
-  imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
-  providers: [ServersService],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  providers: [ServersService, AuthService, AuthGuard],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- Angular-14/Routing/src/app/servers/edit-server/edit-server.component.ts
+++ Angular-14/Routing/src/app/servers/edit-server/edit-server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-server',
@@ -12,18 +12,24 @@ export class EditServerComponent implements OnInit {
   server?: { id: number; name: string; status: string };
   serverName = '';
   serverStatus = '';
+  allowEdit = false;
 
   constructor(
     private serversService: ServersService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) {}
 
   ngOnInit() {
     console.log(this.route.snapshot.queryParams);
     console.log(this.route.snapshot.fragment);
-    this.route.queryParams.subscribe();
+    this.route.queryParams.subscribe((quereParams: Params) => {
+      this.allowEdit = quereParams['allowEdit'] === '1' ? true : false;
+    });
     this.route.fragment.subscribe();
-    const server = this.serversService.getServer(1);
+    const server = this.serversService.getServer(
+      +this.route.snapshot.params['id']
+    );
     if (server) {
       this.server = server;
       this.serverName = server.name;
