55f443865455675a9f342a455e16307985c8d67c
Fuad Hasan <labib.knc@gmail.com>
2025-10-20 16:28:00 +0600

    ngmodel ngform

--- Angular-14/Forms/src/app/app.component.ts
+++ Angular-14/Forms/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, ViewChild } from '@angular/core';
+import { Form, NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -11,13 +11,49 @@ export class AppComponent {
   answer: string = '';
   genders: string[] = ['Male', 'Female'];
   defaultGender: string = this.genders[0];
-  suggestUserName() {
-    const suggestedName = 'Superuser';
+  defaultUserName: string = '';
+  user = {
+    userData: {
+      username: '',
+      email: '',
+    },
+    secret: '',
+    questionAnswer: '',
+    gender: '',
+  };
+  isSubmit: boolean = false;
+  @ViewChild('nameCtrl') ngname!: NgModel;
+  suggestUserName(userForm: NgForm) {
+    // userForm.setValue({
+    //   userData: {
+    //     username: 'Fuad',
+    //     email: '',
+    //   },
+    //   secret: 'teacher',
+    //   questionAnswer: '',
+    //   gender: 'Female',
+    // });
+    // this.defaultUserName = 'Fuad Hasan';
+    userForm.form.patchValue({
+      userData: {
+        username: 'Fuad Hasan',
+      },
+    });
   }
 
   onSubmit(userForm: NgForm) {
-    console.log(userForm);
-    console.log(userForm.value.userData.username);
+    this.isSubmit = true;
+    // this.user.name = userForm.value.userData.username;
+    // this.user.email = userForm.value.userData.email;
+    // this.user.secret = userForm.value.secret;
+    // this.user.questionAnswer = userForm.value.questionAnswer;
+    // this.user.gender = userForm.value.gender;
+
+    this.user = {
+      ...userForm.value,
+      userData: { ...userForm.value.userData },
+    };
+    this.user.userData.username = 'Test';
   }
   clickHere() {
     console.log(this.defaultValue);
