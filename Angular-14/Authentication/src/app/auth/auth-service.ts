719cc1afecb33b1be1d5dcebb14e181c6505238b
Fuad Hasan <labib.knc@gmail.com>
2025-10-23 19:49:31 +0600

    Logging Authentication

--- Angular-14/Authentication/src/app/auth/auth-service.ts
+++ Angular-14/Authentication/src/app/auth/auth-service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { catchError, throwError } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { ErrorHandler, Injectable } from '@angular/core';
+import { BehaviorSubject, catchError, Subject, tap, throwError } from 'rxjs';
+import { User } from './user.mode';
 
 export interface AuthResponseData {
   idToken: string;
@@ -13,6 +14,7 @@ export interface AuthResponseData {
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
+  user = new BehaviorSubject<User | null>(null);
   constructor(private http: HttpClient) {}
   signup(email: string, password: string) {
     return this.http
@@ -25,27 +27,61 @@ export class AuthService {
         }
       )
       .pipe(
-        catchError((errorRes) => {
-          let errorMessage = 'An unknowned error occured!';
-          if (!errorRes.error || !errorRes.error.error) {
-            return throwError(() => new Error(errorMessage));
-          }
-          if (errorRes.error.error.message === 'EMAIL_EXISTS') {
-            errorMessage = 'This email already exists!';
-          }
-          return throwError(() => new Error(errorMessage));
+        catchError(this.handleError),
+        tap((resData) => {
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            new Date(new Date().getTime() + +resData.expiresIn * 1000)
+          );
         })
       );
   }
 
   login(email: string, password: string) {
-    return this.http.post<AuthResponseData>(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDEivxXcoEIRjm6zkN0paIVBaGwmTXPFTo',
-      {
-        email: email,
-        password: password,
-        returnSecureToken: true,
-      }
-    );
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDEivxXcoEIRjm6zkN0paIVBaGwmTXPFTo',
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(
+        catchError(this.handleError),
+        tap((resData) => {
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            new Date(new Date().getTime() + +resData.expiresIn * 1000)
+          );
+        })
+      );
+  }
+
+  private handleAuthentication(
+    email: string,
+    userId: string,
+    token: string,
+    expirationDate: Date
+  ) {
+    const user = new User(email, userId, token, expirationDate);
+    this.user.next(user);
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = 'An unknowned error occured!';
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(() => new Error(errorMessage));
+    }
+    if (errorRes.error.error.message === 'EMAIL_EXISTS') {
+      errorMessage = 'This email already exists!';
+    } else if (errorRes.error.error.message === 'INVALID_LOGIN_CREDENTIALS') {
+      errorMessage = 'Email and Password Mismatch!';
+    }
+    return throwError(() => new Error(errorMessage));
   }
 }
--- Angular-14/Authentication/src/app/auth/auth.component.ts
+++ Angular-14/Authentication/src/app/auth/auth.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthResponseData, AuthService } from './auth-service';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -12,7 +13,7 @@ export class AuthComponent {
   spinning = false;
   error!: string | null;
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
@@ -36,6 +37,7 @@ export class AuthComponent {
         next: (response) => {
           console.log(response);
           this.spinning = false;
+          this.router.navigate(['/recipes']);
         },
         error: (errorMessage) => {
           console.log(errorMessage);
